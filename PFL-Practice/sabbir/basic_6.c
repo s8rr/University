9b43abd53c093c0fae091a0688f864f9c92f6daa
Sabbir <93143691+s8rr@users.noreply.github.com>
2026-08-06 00:19:52 +0600

    Create basic_6.c

---
+++ PFL-Practice/sabbir/basic_6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float l, b, a;
+    printf("Enter the length of the rectangle: ");
+    scanf("%f", &length);
+    printf("Enter the breadth of the rectangle: ");
+    scanf("%f", &breadth);
+    a = l * b;
+    printf("The area of the rectangle is: %.2f\n", a);
+}
