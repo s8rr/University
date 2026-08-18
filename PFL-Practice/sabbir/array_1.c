934208ad7c93a6b7652baa20db86f116738f1a3f
Sabbir <93143691+s8rr@users.noreply.github.com>
2026-08-18 19:02:52 +0600

    Create array_1.c

---
+++ PFL-Practice/sabbir/array_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int i, n , x[7],sum=0;
+    printf("Enter a Number: ");
+    scanf("%d",&n);
+    printf("Enter What index you want to Exclude:");
+    scanf("%d",&k);
+
+    for(i=0;i<n;i++){
+        scanf("%d",&x[i]);
+    }
+    for(i=0;i<n;i++){
+            sum+=x[i];
+    }
+    printf("Sum is %d",sum);
+    return 0;
+}
