85ce9dd699279d72cbd8c2c973f449d474d82276
Sabbir <93143691+s8rr@users.noreply.github.com>
2026-08-19 22:49:43 +0600

    Update array_6.c

--- PFL-Practice/sabbir/array_6.c
+++ PFL-Practice/sabbir/array_6.c
@@ -15,22 +15,16 @@ int main() {
     printf("Enter the position of the element to delete (1 to %d): ", size);
     scanf("%d", &position);
 
-    if (position < 1 || position > size) {
-        printf("Invalid position! Deletion is not possible.\n");
-    } else {
-
-        for (i = position - 1; i < size - 1; i++) {
+    for (i = position - 1; i < size - 1; i++) {
             arr[i] = arr[i + 1];
-        }
-
-        size--;
-
-        printf("Resultant array after deletion:\n");
-        for (i = 0; i < size; i++) {
-            printf("%d ", arr[i]);
-        }
-        printf("\n");
     }
+    size--;
+
+    printf("Resultant array after deletion:\n");
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 
     return 0;
 }
