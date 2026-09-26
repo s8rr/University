32a40002595c938c1716160d08b20e7899d4bfe3
Sabbir <93143691+s8rr@users.noreply.github.com>
2026-09-27 01:02:27 +0600

    Update assignment_f.c

--- PFL-Practice/assignment_f.c
+++ PFL-Practice/assignment_f.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int trainSwapping(int a[], int n) {
+    int i, j, t, swaps = 0;
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n - i - 1; j++) {
+            if(a[j] > a[j + 1]) {
+                t = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = t;
+                swaps++;
+            }
+        }
+    }
+    return swaps;
+}
+
+int main() {
+    int T, n, i, a[50], swaps;
+    scanf("%d", &T);
+
+    while(T--) {
+        scanf("%d", &n);
+        for(i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+        }
+        swaps = trainSwapping(a, n);
+
+        printf("Optimal train swapping takes %d swaps.\n", swaps);
+    }
+    return 0;
+}
